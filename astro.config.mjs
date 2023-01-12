f76a8c1138c59fd87614c09b95daa981260ad245
DMurauyova <dmurauyova@dxagency.com>
2023-01-12 15:16:40 -0500

    test

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  output: 'server',
+  integrations: [mdx(), sitemap(), react(), tailwind()],
+  adapter: netlify()
+});
---
+++ src/components/Form.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function App() {
+   const {
+      register,
+      handleSubmit,
+      formState: { errors },
+   } = useForm();
+
+   const onSubmit = (data) => {
+      console.log(data);
+   };
+
+   return (
+      <div className='App'>
+         <form
+            name='contact'
+            onSubmit={handleSubmit(onSubmit)}
+            data-netlify='true'
+            data-netlify-honeypot='bot-field'
+         >
+            <div className='form-control'>
+               <label>Email</label>
+               <input
+                  type='text'
+                  name='email'
+                  {...register("email", {
+                     required: true,
+                     pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                  })}
+               />
+               {errors.email && errors.email.type === "required" && (
+                  <p className='errorMsg'>Email is required.</p>
+               )}
+               {errors.email && errors.email.type === "pattern" && (
+                  <p className='errorMsg'>Email is not valid.</p>
+               )}
+            </div>
+            <div className='form-control'>
+               <label>Password</label>
+               <input
+                  type='password'
+                  name='password'
+                  {...register("password", {
+                     required: true,
+                     minLength: 6,
+                  })}
+               />
+               {errors.password && errors.password.type === "required" && (
+                  <p className='errorMsg'>Password is required.</p>
+               )}
+               {errors.password && errors.password.type === "minLength" && (
+                  <p className='errorMsg'>
+                     Password should be at-least 6 characters.
+                  </p>
+               )}
+            </div>
+            <div className='form-control'>
+               <label></label>
+               <button type='submit'>Login</button>
+            </div>
+         </form>
+      </div>
+   );
+}
